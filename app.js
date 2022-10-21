2ac04e3dbbe5782815ca013ca0d1071e489755d9
helloworldgreg <97385650+helloworldgreg@users.noreply.github.com>
2022-10-21 17:00:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const app = express();
 const port = 80;
 
 app.get('/', (req, res) => {
-  res.send('Hello World, you are using the root route');
+  res.send('Hello World, you are using the MAIN route');
 });
 app.get('/test', (req, res) => {
-  res.send('Hello World, you are using the test route ');
+  res.send('Hello World, you are using the TEST route ');
 });
 
 app.listen(port, () => {
