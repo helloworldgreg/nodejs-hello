bbf425379e665ae970b0297a510e8774dfd72945
helloworldgreg <97385650+helloworldgreg@users.noreply.github.com>
2022-10-24 13:13:55 +0200

    Added route for CloudFront

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
 app.get('/test', (req, res) => {
   res.send('Hello World, you are using the TEST route ');
 });
+app.get('/cloudfront', (req, res) => {
+  let r = (Math.random() + 1).toString(36).substring(7);
+  res.send('Hello World, you are using the CLOUDFRONT route ' + r);
+  res.set('Cache-Control', 'max-age=120');
+});
 
 app.listen(port, () => {
   console.log('Example app listening on port ${port}');
